d9838e626de40d83e99454b76a3dc1ceac3cd7a6
Morgan Benton <morgan.benton@gmail.com>
2021-09-22 13:50:54 -0400

    add fizz and test

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,3 +1,11 @@
 '''
 A fizzbuzz program
 '''
+
+def fizz(x):
+  '''
+  Takes a single input, returns 'fizz' if that
+  input is a multiple of 3, and just returns the
+  input "as is" if it is not a multiple of 3.
+  '''
+  return 'fizz' if x % 3 == 0 else x
--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -2,6 +2,9 @@
 Unit tests for the fizzbuzz program
 '''
 
+# import the code to be tested
+from fizzbuzz import fizz
+
 def describe_a_fizzbuzz_program_that():
 
     def has_a_smoke_test():
@@ -13,4 +16,13 @@ def describe_a_fizzbuzz_program_that():
         '''
         assert True
 
-    
+    def takes_a_numeric_input_and_returns_fizz_if_it_is_a_multiple_of_3():
+        '''
+        We need a function that can take a single input and that
+        returns a value which could either be "fizz" or just whatever
+        the input was to begin with.
+        '''
+        assert fizz(3) == 'fizz'
+        assert fizz(4) == 4
+        assert fizz(9) == 'fizz'
+        assert fizz(17) == 17
