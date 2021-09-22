699cf7f99ec3b84bcb304ee2bbb1a2ec6e8967ec
Morgan Benton <morgan.benton@gmail.com>
2021-09-22 13:26:43 -0400

    setup testing and smoke test

---
+++ fizzbuzz.py
@@ -0,0 +1,3 @@
+'''
+A fizzbuzz program
+'''
---
+++ fizzbuzz_test.py
@@ -0,0 +1,16 @@
+'''
+Unit tests for the fizzbuzz program
+'''
+
+def describe_a_fizzbuzz_program_that():
+
+    def has_a_smoke_test():
+        '''
+        The purpose of a smoke test is to give us
+        confidence that our testing infrastructure
+        is set up correctly. We do that by creating
+        a test that should ALWAYS pass.
+        '''
+        assert True
+
+    
