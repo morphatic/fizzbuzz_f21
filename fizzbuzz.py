13926d5018a1450201fed9db62926ee46ef4668a
Morgan Benton <morgan.benton@gmail.com>
2021-10-01 13:51:32 -0400

    added test for numeric

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,13 +2,16 @@
 A fizzbuzz program
 '''
 
+# utility class for determining if a variable is numeric
+from numbers import Number
+
 def fizz(x):
   '''
   Takes a single input, returns 'fizz' if that
   input is a multiple of 3, and just returns the
   input "as is" if it is not a multiple of 3.
   '''
-  return 'fizz' if x % 3 == 0 else x
+  return 'fizz' if isinstance(x, Number) and x % 3 == 0 else x
 
 def buzz(x):
   '''
@@ -16,4 +19,15 @@ def buzz(x):
   input is a multiple of 5, and just returns the
   input "as is" if it is not a multiple of 5.
   '''
-  return 'buzz' if x % 5 == 0 else x
+  return 'buzz' if isinstance(x, Number) and x % 5 == 0 else x
+
+def fibu(x):
+  '''
+  Takes a single input, returns 'fizzbuzz' if that
+  input is a multiple of 15, and just returns the
+  input "as is" if it is not a multiple of 15.
+  '''
+  return 'fizzbuzz' if isinstance(x, Number) and x % 15 == 0 else x
+
+for x in range(1, 101):
+  print(fizz(buzz(fibu(x))))
--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -3,7 +3,7 @@ Unit tests for the fizzbuzz program
 '''
 
 # import the code to be tested
-from fizzbuzz import fizz, buzz
+from fizzbuzz import fizz, buzz, fibu
 
 def describe_a_fizzbuzz_program_that():
 
@@ -23,6 +23,7 @@ def describe_a_fizzbuzz_program_that():
         the input was to begin with.
         '''
         assert fizz(3) == 'fizz'
+        assert fizz(3.0) == 'fizz'
         assert fizz(4) == 4
         assert fizz(9) == 'fizz'
         assert fizz(17) == 17
@@ -36,8 +37,29 @@ def describe_a_fizzbuzz_program_that():
         the input was to begin with.
         '''
         assert buzz(5) == 'buzz'
+        assert buzz(5.0) == 'buzz'
         assert buzz(4) == 4
         assert buzz(10) == 'buzz'
         assert buzz(17) == 17
         assert buzz(15) == 'buzz'
         assert buzz(3) == 3
+
+    def takes_a_numeric_input_and_returns_fizzbuzz_if_it_is_a_multiple_of_15():
+        '''
+        We need a function that can take a single input and that
+        returns a value which could either be "fizzbuzz" or just whatever
+        the input was to begin with.
+        '''
+        assert fibu(15) == 'fizzbuzz'
+        assert fibu(3) == 3
+        assert fibu(4) == 4
+        assert fibu(5) == 5
+
+    def passes_non_numeric_inputs_through_as_is():
+        '''
+        If the input to fizz(), buzz(), and fibu() is non-numeric,
+        return the value "as is" without transform
+        '''
+        assert fizz('abc') == 'abc'
+        assert buzz('abc') == 'abc'
+        # assert fibu('abc') == 'abc'
